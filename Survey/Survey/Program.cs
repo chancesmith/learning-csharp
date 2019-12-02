879d37f7f4a349bda837456d7e2c4f89bcbe0591
Chance Smith <chancesmithb@gmail.com>
2019-12-02 07:26:45 -0600

    📦  NEW: [Survey] add if logic

--- Survey/Survey/Program.cs
+++ Survey/Survey/Program.cs
@@ -9,17 +9,25 @@ namespace Survey
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
 
-            Console.WriteLine("Your name is {0}", name);
 
             Console.WriteLine("What is your age?");
             var age = Console.ReadLine();
 
-            Console.WriteLine("Your age is {0}", age);
 
             Console.WriteLine("What is your birth month?");
             var month = Console.ReadLine();
 
-            Console.WriteLine("Your birth month is {0}", age);
+            Console.WriteLine("Your name is {0}", name);
+            Console.WriteLine("Your age is {0}", age);
+            Console.WriteLine("Your birth month is {0}", month);
+
+            if (month == "march") {
+                Console.WriteLine("You are Aries.");
+            } else if (month == "april") {
+                Console.WriteLine("You are Taurus.");
+            } else if (month == "may") {
+                Console.WriteLine("You are Gemini.");
+            }
         }
     }
 }
