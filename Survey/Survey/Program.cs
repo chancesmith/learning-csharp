d5dab027ee9a73ca99dcd4bd61953e2582573ba5
Chance Smith <chancesmithb@gmail.com>
2019-12-02 07:20:56 -0600

    📦  NEW: moved projects into each folder

---
+++ Survey/Survey/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Survey
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Your name is {0}", name);
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("Your age is {0}", age);
+
+            Console.WriteLine("What is your birth month?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your birth month is {0}", age);
+        }
+    }
+}
