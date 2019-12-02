7795d3c15880aa19615adb271f2ee0a6b529e09a
Chance Smith <chancesmithb@gmail.com>
2019-12-02 07:13:13 -0600

    add survey challenge

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -6,8 +6,18 @@ namespace helloworld
     {
         public static void Main(string[] args)
         {
-            var anything = Console.ReadLine();
-            Console.WriteLine(anything);
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What is your birth month?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is, " + name);
+            Console.WriteLine("Your age is, " + age);
+            Console.WriteLine("Your birth month is, " + month);
         }
     }
 }
