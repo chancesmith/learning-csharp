9ba951602518c448070df5442fe8029b615c3c90
Chance Smith <chancesmithb@gmail.com>
2019-12-02 07:32:18 -0600

    📦  NEW: add PassCode challenge

---
+++ PassCode/PassCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PassCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter you passcode:");
+            var passcode = Console.ReadLine();
+
+            if (passcode == "secret")
+            {
+                Console.WriteLine("You are authenticated.");
+            }
+            else
+            {
+                Console.WriteLine("You are dead to me.");
+            }
+        }
+    }
+}
